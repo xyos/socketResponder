22618145fff068f1686a08f134cfae4d9c94d4cb
Jairo Suarez <jasuarezpr@gmail.com>
2015-06-09 03:19:12 -0500

    deleted some crap

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('change', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
